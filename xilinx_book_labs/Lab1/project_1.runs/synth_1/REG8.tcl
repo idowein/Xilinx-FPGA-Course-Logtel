69b695c6da9d45775abb496a2f1513b6bde7f473
idowein <ido.weinstock@gmail.com>
2025-01-14 21:10:13 +0200

    coding REG8_V8

--- xilinx_book_labs/Lab1/project_1.runs/synth_1/REG8.tcl
+++ xilinx_book_labs/Lab1/project_1.runs/synth_1/REG8.tcl
@@ -22,14 +22,14 @@ create_project -in_memory -part xc7z020clg400-1
 set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
-set_property webtalk.parent_dir {C:/Users/idowe/FPGA Projects/FPGA-Course-Logtel/project_1/project_1.cache/wt} [current_project]
-set_property parent.project_path {C:/Users/idowe/FPGA Projects/FPGA-Course-Logtel/project_1/project_1.xpr} [current_project]
+set_property webtalk.parent_dir {C:/Users/idowe/FPGA Projects/FPGA-Course-Logtel/xilinx_book_labs/Lab1/project_1.cache/wt} [current_project]
+set_property parent.project_path {C:/Users/idowe/FPGA Projects/FPGA-Course-Logtel/xilinx_book_labs/Lab1/project_1.xpr} [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language VHDL [current_project]
 set_property board_part digilentinc.com:zybo-z7-20:part0:1.2 [current_project]
-set_property ip_output_repo {c:/Users/idowe/FPGA Projects/FPGA-Course-Logtel/project_1/project_1.cache/ip} [current_project]
+set_property ip_output_repo {c:/Users/idowe/FPGA Projects/FPGA-Course-Logtel/xilinx_book_labs/Lab1/project_1.cache/ip} [current_project]
 set_property ip_cache_permissions {read write} [current_project]
-read_vhdl -library xil_defaultlib {{C:/Users/idowe/FPGA Projects/FPGA-Course-Logtel/project_1/project_1.srcs/sources_1/new/REG8.vhd}}
+read_vhdl -library xil_defaultlib {{C:/Users/idowe/FPGA Projects/FPGA-Course-Logtel/xilinx_book_labs/Lab1/project_1.srcs/sources_1/new/REG8.vhd}}
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
 # design are intentionally left as such for best results. Dcp files will be
--- xilinx_book_labs/Lab1/project_1.runs/synth_1/runme.sh
+++ xilinx_book_labs/Lab1/project_1.runs/synth_1/runme.sh
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='C:/Users/idowe/FPGA Projects/FPGA-Course-Logtel/project_1/project_1.runs/synth_1'
+HD_PWD='C:/Users/idowe/FPGA Projects/FPGA-Course-Logtel/xilinx_book_labs/Lab1/project_1.runs/synth_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
