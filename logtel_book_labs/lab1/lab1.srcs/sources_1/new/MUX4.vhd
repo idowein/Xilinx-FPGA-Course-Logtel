b833cd58a1717b76afa118fc8a58ec3c94cd526a
idowein <ido.weinstock@gmail.com>
2025-01-14 22:07:56 +0200

    changing the name of mux to mux4

--- logtel_book_labs/lab1/lab1.srcs/sources_1/new/MUX4.vhd
+++ logtel_book_labs/lab1/lab1.srcs/sources_1/new/MUX4.vhd
@@ -4,7 +4,7 @@
 -- 
 -- Create Date: 01/14/2025 09:24:07 PM
 -- Design Name: 
--- Module Name: MUX - Behavioral
+-- Module Name: MUX4 - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -31,16 +31,16 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity MUX is
+entity MUX4 is
     Port ( IN1 : in STD_LOGIC;
            IN2 : in STD_LOGIC;
            IN3 : in STD_LOGIC;
            IN4 : in STD_LOGIC;
            SELECTOR : in STD_LOGIC_VECTOR (1 downto 0);
            RES : out STD_LOGIC);
-end MUX;
+end MUX4;
 
-architecture Behavioral of MUX is
+architecture Behavioral of MUX4 is
 
 begin
 -- MUX4
