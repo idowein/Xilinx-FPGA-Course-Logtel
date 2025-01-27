a1bc8be1097bbd99f166d10454843ca747e5ce37
idowein <ido.weinstock@gmail.com>
2025-01-27 19:45:06 +0200

    commenting

--- logtel_book_labs/lab8/lab8.srcs/sources_1/new/traffic_light.vhd
+++ logtel_book_labs/lab8/lab8.srcs/sources_1/new/traffic_light.vhd
@@ -52,89 +52,89 @@ signal count: STD_LOGIC_VECTOR (11 downto 0) := (others =>'0');
 
 begin
 
-PROCESS (CLK)
-    begin
-        if CLK = '1' and CLK' event then
-            if (RST = '1') then
-                count <= (others => '0');
-            elsif count = 2400 then
-                count <= (others => '0');
-             else
-                count <= count + 1;
+    PROCESS (CLK)
+        begin
+            if CLK = '1' and CLK' event then
+                if (RST = '1') then
+                    count <= (others => '0');
+                elsif count = 2400 then
+                    count <= (others => '0');
+                 else
+                    count <= count + 1;
+                 end if;
              end if;
-         end if;
-         
-    end PROCESS;
-
-
-PROCESS (CLK)
-    begin 
-        if CLK = '1' and CLK' event then
-            if (RST = '1') then  
-            state <= START;
-            RED_W <= '1';
-            GREEN_W <= '0';
-            RED_C <= '1';
-            ORANGE_C <= '0';
-            GREEN_C <= '0';
-            else
-                case state is 
-                    when START => 
-                            RED_W <= '1';
-                            GREEN_W <= '0';
-                            RED_C <= '1';
-                            ORANGE_C <= '0';
-                            GREEN_C <= '0';
-                            if count = 100 then
-                                state <= WALK;
-                            end if;
-                    when WALK => 
-                            RED_W <= '0';
-                            GREEN_W <= '1';
-                            RED_C <= '1';
-                            ORANGE_C <= '0';
-                            GREEN_C <= '0';
-                            if count = 100 then
-                                state <= STOP_WALK;
-                            end if;
-                    when STOP_WALK => 
-                            RED_W <= '1';
-                            GREEN_W <= '0';
-                            RED_C <= '1';
-                            ORANGE_C <= '0';
-                            GREEN_C <= '0';
-                            if count = 100 then
-                                state <= PRE_DRIVE;
-                            end if;
-                    when PRE_DRIVE => 
-                            RED_W <= '1';
-                            GREEN_W <= '0';
-                            RED_C <= '0';
-                            ORANGE_C <= '1';
-                            GREEN_C <= '0';
-                            if count = 100 then
-                                state <= DRIVE;
-                            end if;                
-                    when DRIVE => 
-                            RED_W <= '1';
-                            GREEN_W <= '0';
-                            RED_C <= '0';
-                            ORANGE_C <= '0';
-                            GREEN_C <= '1';
-                            if count = 100 then
-                                state <= DRIVE;
-                            end if; 
-                    when STOP_DRIVE => 
-                            RED_W <= '1';
-                            GREEN_W <= '0';
-                            RED_C <= '1';
-                            ORANGE_C <= '0';
-                            GREEN_C <= '0';
-                            if count = 100 then
-                                state <= START;
-                            end if;  
-            end case;
-            end if;
-    end if;                                 
-end PROCESS;         
+             
+        end PROCESS;
+    
+    
+    PROCESS (CLK)
+        begin 
+            if CLK = '1' and CLK' event then
+                if (RST = '1') then  
+                state <= START;
+                RED_W <= '1';
+                GREEN_W <= '0';
+                RED_C <= '1';
+                ORANGE_C <= '0';
+                GREEN_C <= '0';
+                else
+                    case state is 
+                        when START => 
+                                RED_W <= '1';
+                                GREEN_W <= '0';
+                                RED_C <= '1';
+                                ORANGE_C <= '0';
+                                GREEN_C <= '0';
+                                if count = 100 then
+                                    state <= WALK;
+                                end if;
+                        when WALK => 
+                                RED_W <= '0';
+                                GREEN_W <= '1';
+                                RED_C <= '1';
+                                ORANGE_C <= '0';
+                                GREEN_C <= '0';
+                                if count = 100 then
+                                    state <= STOP_WALK;
+                                end if;
+                        when STOP_WALK => 
+                                RED_W <= '1';
+                                GREEN_W <= '0';
+                                RED_C <= '1';
+                                ORANGE_C <= '0';
+                                GREEN_C <= '0';
+                                if count = 100 then
+                                    state <= PRE_DRIVE;
+                                end if;
+                        when PRE_DRIVE => 
+                                RED_W <= '1';
+                                GREEN_W <= '0';
+                                RED_C <= '0';
+                                ORANGE_C <= '1';
+                                GREEN_C <= '0';
+                                if count = 100 then
+                                    state <= DRIVE;
+                                end if;                
+                        when DRIVE => 
+                                RED_W <= '1';
+                                GREEN_W <= '0';
+                                RED_C <= '0';
+                                ORANGE_C <= '0';
+                                GREEN_C <= '1';
+                                if count = 100 then
+                                    state <= DRIVE;
+                                end if; 
+                        when STOP_DRIVE => 
+                                RED_W <= '1';
+                                GREEN_W <= '0';
+                                RED_C <= '1';
+                                ORANGE_C <= '0';
+                                GREEN_C <= '0';
+                                if count = 100 then
+                                    state <= START;
+                                end if;  
+                        end case;
+                end if;
+        end if;                                 
+    end PROCESS;         
 end Behavioral;
