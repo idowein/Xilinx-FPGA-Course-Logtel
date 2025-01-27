1ff12cf25ca701574119170da6799b5835a98fca
idowein <ido.weinstock@gmail.com>
2025-01-27 14:02:01 +0200

    continuing level C of lab 5

--- logtel_book_labs/lab5/lab5.srcs/sim_1/new/blk_mem_gen_1_tb.vhd
+++ logtel_book_labs/lab5/lab5.srcs/sim_1/new/blk_mem_gen_1_tb.vhd
@@ -23,65 +23,89 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use IEEE.std_logic_unsigned.all;
 
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity blk_mem_gen_1_tb is
---  Port ( );
 end blk_mem_gen_1_tb;
 
 architecture Behavioral of blk_mem_gen_1_tb is
 
-COMPONENT blk_mem_gen_1
-  PORT (
-    clka : IN STD_LOGIC;
-    ena : IN STD_LOGIC;
-    wea : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
-    addra : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
-    dina : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-    clkb : IN STD_LOGIC;
-    enb : IN STD_LOGIC;
-    addrb : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
-    doutb : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
-  );
-END COMPONENT;
+    COMPONENT blk_mem_gen_1
+    PORT (
+        clka : IN STD_LOGIC;
+        ena : IN STD_LOGIC;
+        wea : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
+        addra : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+        dina : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        clkb : IN STD_LOGIC;
+        enb : IN STD_LOGIC;
+        addrb : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+        doutb : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+    );
+    END COMPONENT;
 
-signal clka : STD_LOGIC := '0';
-signal ena :  STD_LOGIC := '0';
-signal wea :  STD_LOGIC_VECTOR(0 DOWNTO 0) := (others => '1');
-signal addra : STD_LOGIC_VECTOR(9 DOWNTO 0) := (others => '1');
-signal dina : STD_LOGIC_VECTOR(7 DOWNTO 0):= (others => '0');
-signal clkb : STD_LOGIC := '0';
-signal enb : STD_LOGIC := '0';
-signal addrb : STD_LOGIC_VECTOR(9 DOWNTO 0) := (others => '1');
-doutb : STD_LOGIC_VECTOR(7 DOWNTO 0) := '0';
+    -- Signals
+    signal clka : STD_LOGIC := '0';
+    signal ena :  STD_LOGIC := '1'; -- Enable the write port
+    signal wea :  STD_LOGIC_VECTOR(0 DOWNTO 0) := (others => '1'); -- Write enable
+    signal addra : STD_LOGIC_VECTOR(9 DOWNTO 0) := (others => '0'); -- Write address
+    signal dina : STD_LOGIC_VECTOR(7 DOWNTO 0) := (others => '0'); -- Data input for write
+    signal clkb : STD_LOGIC := '0';
+    signal enb : STD_LOGIC := '1'; -- Enable the read port
+    signal addrb : STD_LOGIC_VECTOR(9 DOWNTO 0) := (others => '0'); -- Read address
+    signal doutb : STD_LOGIC_VECTOR(7 DOWNTO 0); -- Data output for read
 
 begin
 
-    your_instance_name : blk_mem_gen_1
-      PORT MAP (
-        clka => clka,
-        ena => ena,
-        wea => wea,
-        addra => addra,
-        dina => dina,
-        clkb => clkb,
-        enb => enb,
-        addrb => addrb,
-        doutb => doutb
-      );
-      
-     -- clock generation
-    clka <= not(clka) after 5ns;
-    
-    
-  
+    -- Instantiate the DUT (Device Under Test)
+    uut: blk_mem_gen_1
+        PORT MAP (
+            clka => clka,
+            ena => ena,
+            wea => wea,
+            addra => addra,
+            dina => dina,
+            clkb => clkb,
+            enb => enb,
+            addrb => addrb,
+            doutb => doutb
+        );
+
+    -- Generate clock signals for the DUT
+    clock_process: process
+    begin
+        while true loop
+            clka <= '1';
+            clkb <= '1';
+            wait for 5 ns;
+            clka <= '0';
+            clkb <= '0';
+            wait for 5 ns;
+        end loop;
+    end process;
+
+    -- Testbench stimulus process
+    stimulus_process: process
+    begin
+        -- Write data to all memory locations
+        for i in 0 to 255 loop 
+            addra <= std_logic_vector(to_unsigned(i, addra'length)); 
+            dina <= std_logic_vector(to_unsigned(2 * i, dina'length)); 
+            wea(0) <= '1'; -- Enable write
+            wait for 10 ns; -- Wait for one clock cycle
+            wea(0) <= '0'; -- Disable write
+            wait for 10 ns; 
+        end loop;
+
+        -- Read data from all memory locations and verify
+        for i in 0 to 255 loop 
+            addrb <= std_logic_vector(to_unsigned(i, addrb'length)); 
+            wait for 10 ns; -- Wait for one clock cycle 
+            assert doutb = std_logic_vector(to_unsigned(2 * i, doutb'length)) 
+            report "Read error at address " & integer'image(to_integer(unsigned(addrb))) 
+            severity error; 
+            wait for 10 ns; 
+        end loop;
+
+        wait; -- End of simulation
+    end process;
 
 end Behavioral;
