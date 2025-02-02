2706354c8e938a18ce89ef1483df622499cd243c
idowein <ido.weinstock@gmail.com>
2025-02-02 12:55:59 +0200

    component declarations in histogram vhd

--- logtel_book_labs/lab7/lab7.srcs/sources_1/new/histogram_median_unit.vhd
+++ logtel_book_labs/lab7/lab7.srcs/sources_1/new/histogram_median_unit.vhd
@@ -47,16 +47,27 @@ end histogram_median_unit;
 
 architecture Behavioral of histogram_median_unit is
 
-    -- Instantiate BRAM for histogram storage
-    histogram_bram: blk_mem_gen_0 
-        Port map (
-            clka => clk, 
-            wea => wea, 
-            addra => std_logic_vector(current_value), 
-            dina => (others => '1'), 
-            douta => ram_dout 
-        );  
-
+    component single_port_rom -- ROM component declaration
+        port(
+            clka  : IN STD_LOGIC;
+            addra : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+            douta : OUT STD_LOGIC_VECTOR(7 DOWNTO 0) 
+        );
+    end component;
+    
+    component dual_port_ram -- RAN declaration
+      port (
+        clka : IN STD_LOGIC;
+        wea : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
+        addra : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        dina : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+        clkb : IN STD_LOGIC;
+        enb : IN STD_LOGIC;
+        addrb : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        doutb : OUT STD_LOGIC_VECTOR(9 DOWNTO 0)
+      );
+    end component;
+    
 begin
 
 
