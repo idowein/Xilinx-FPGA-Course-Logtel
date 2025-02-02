dded8c9c45da2bc67574494bbe4d24d8ba443d13
idowein <ido.weinstock@gmail.com>
2025-02-02 12:29:58 +0200

    generating ROM and RAM using IP catalog

---
+++ logtel_book_labs/lab7/lab7.srcs/sources_1/ip/single_port_rom/single_port_rom_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+-- Date        : Sun Feb  2 11:58:35 2025
+-- Host        : Ido running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub {c:/Users/idowe/FPGA
+--               Projects/FPGA-Course-Logtel/logtel_book_labs/lab7/lab7.srcs/sources_1/ip/single_port_rom/single_port_rom_stub.vhdl}
+-- Design      : single_port_rom
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity single_port_rom is
+  Port ( 
+    clka : in STD_LOGIC;
+    addra : in STD_LOGIC_VECTOR ( 9 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 7 downto 0 )
+  );
+
+end single_port_rom;
+
+architecture stub of single_port_rom is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,addra[9:0],douta[7:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_4,Vivado 2019.2";
+begin
+end;
