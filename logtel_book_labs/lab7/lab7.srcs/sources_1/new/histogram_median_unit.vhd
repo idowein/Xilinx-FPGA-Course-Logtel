c6e5417fd0bd8e1477652ed217f5a3f6f97c24e3
idowein <ido.weinstock@gmail.com>
2025-02-05 14:52:49 +0200

    phase 1 and phase 2

--- logtel_book_labs/lab7/lab7.srcs/sources_1/new/histogram_median_unit.vhd
+++ logtel_book_labs/lab7/lab7.srcs/sources_1/new/histogram_median_unit.vhd
@@ -82,7 +82,7 @@ architecture Behavioral of histogram_median_unit is
     signal ram_wea : STD_LOGIC_VECTOR(0 DOWNTO 0) := (others => '0'); -- write enable initializing 
     
     -- external signals declaration
-    signal data_counter : STD_LOGIC_VECTOR (9 downto 0) := (others => '0'); 
+    signal data_counter : STD_LOGIC_VECTOR (11 downto 0) := (others => '0'); 
     signal hist_index   : STD_LOGIC_VECTOR(7 downto 0) := (others => '0'); 
     signal median_number_counter   : STD_LOGIC_VECTOR(9 downto 0) := (others => '0'); 
     signal hist_full    : STD_LOGIC := '0';
@@ -97,7 +97,7 @@ begin
             if rst = '1' then
                 data_counter <= (others => '0');
             else
-                if data_counter < 1023 then 
+                if data_counter < 2558 then -- number of clock periods of 3 phases ( collect, prresent, reset)
                     data_counter <= data_counter + 1; 
                 end if;
             end if;
@@ -109,44 +109,49 @@ begin
     begin
         if rst = '1' then
             ram_address_a <= (others => '0'); 
+            ram_address_b <= (others => '0'); 
             hist_full <= '0';
         elsif rising_edge(clk) then
-            if data_counter < 1023 then 
+            if data_counter < 2046 then -- phase 1 : reading & writing
                 -- Read from ROM
-                rom_address <= data_counter; 
+                rom_address <= data_counter(10 downto 1);  -- we don't use the MSB and LSB of the data_counter (becuase we are working in 50mhz, half the frequency than the rom sending data)
                 -- Write to RAM
                 ram_address_a <= rom_dout; 
-                ram_dina <= ram_dout + '1'; 
-                ram_wea(0) <= '1'; 
-            else
+                ram_address_b <= rom_dout; 
+                ram_dina <= ram_dout + 1; 
+                ram_wea(0) <= data_counter(0); -- ram_wea getting counter in 100 mhz
+                hist_full <= '0'; 
+            elsif data_counter <= 2302 then -- phase 2 : reading
                 hist_full <= '1'; 
                 ram_wea(0) <= '0'; 
+--                ram_address_b <= data_counter - 2302; -- the first of reading will be 0
+                ram_address_b <= std_logic_vector(to_unsigned((to_integer(unsigned(data_counter)) - 2302), 8));
             end if;
         end if;
     end process;
     
-    -- Read from RAM and Assign to HISTOGRAM unit Output Signals
-    process (clk, rst)
-    begin
-        if rst = '1' then
-            ram_address_b <= (others => '0'); 
-            hist_index <= (others => '0'); 
-            hist_amount <= (others => '0'); 
-            ram_read_enable <= '0'; 
-        elsif rising_edge(clk) then
-            if hist_full = '1' and not ram_read_enable = '1' then 
-                ram_read_enable <= '1'; 
-            end if;
-            if ram_read_enable = '1' then 
-                hist_index <= ram_address_b; 
-                hist_amount <= ram_dout; 
-                ram_address_b <= ram_address_b + 1; 
-                if ram_address_b = 255 then -- stop reading from RAM
-                    ram_read_enable <= '0'; 
-                end if;
-            end if;
-        end if;
-    end process;
+--    -- Read from RAM and Assign to HISTOGRAM unit Output Signals
+--    process (clk, rst)
+--    begin
+--        if rst = '1' then
+            
+--            hist_index <= (others => '0'); 
+--            hist_amount <= (others => '0'); 
+--            ram_read_enable <= '0'; 
+--        elsif rising_edge(clk) then
+--            if hist_full = '1' and not ram_read_enable = '1' then 
+--                ram_read_enable <= '1'; 
+--            end if;
+--            if ram_read_enable = '1' then 
+--                hist_index <= ram_address_b; 
+--                hist_amount <= ram_dout; 
+                
+--                if ram_address_b = 255 then -- stop reading from RAM
+--                    ram_read_enable <= '0'; 
+--                end if;
+--            end if;
+--        end if;
+--    end process;
  
     -- device under unit (DUT) is ROM 
     DUT_ROM : single_port_rom
