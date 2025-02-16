d67dff163876671372391e994fab13d1e3c6c4c4
idowein <ido.weinstock@gmail.com>
2025-02-16 22:53:48 +0200

    writing tb - simulation doesnt behave has exepted

--- logtel_book_labs/class_labs/DSP_EXERCISE/DSP_EXERCISE.srcs/sim_1/new/MULTIPLIER_UNIT_TB.vhd
+++ logtel_book_labs/class_labs/DSP_EXERCISE/DSP_EXERCISE.srcs/sim_1/new/MULTIPLIER_UNIT_TB.vhd
@@ -1,40 +1,9 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/13/2025 12:47:18 PM
--- Design Name: 
--- Module Name: MULTIPLIER_UNIT_TB - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use IEEE.std_logic_unsigned.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity MULTIPLIER_UNIT_TB is
---  Port ( );
 end MULTIPLIER_UNIT_TB;
 
 architecture Behavioral of MULTIPLIER_UNIT_TB is
@@ -75,11 +44,57 @@ begin
       DATA_OUT_P => DATA_OUT_P 
     );
 
-    -- Clock generation process
-    CLK <= not CLK after 5 ns;
+  -- Clock generation process
+  process
+  begin
+      while true loop
+          CLK <= '0';
+          wait for CLK_PERIOD / 2;
+          CLK <= '1';
+          wait for CLK_PERIOD / 2;
+      end loop;
+  end process;
 
-    -- Reset generatrion
-    RST <= '1', '0' after CLK_PERIOD * 10;
+  -- Reset process
+  process
+  begin
+      RST <= '1';
+      wait for 50 ns;
+      RST <= '0';
+      wait;
+  end process;
+
+  -- Stimulus process
+  STIMULUS_PROCESS: PROCESS
+  BEGIN
+      WAIT FOR 60 ns; -- Ensure reset completes first
+      
+      -- First round (A + B + 0)
+      DATA_IN_A <= std_logic_vector(to_unsigned(1, 25)); 
+      DATA_IN_B <= std_logic_vector(to_unsigned(1, 18));
+      WAIT FOR CLK_PERIOD;
+      
+      -- Second round (A + B + Previous Sum)
+      WAIT FOR CLK_PERIOD;
+      DATA_IN_A <= std_logic_vector(to_unsigned(2, 25));
+      DATA_IN_B <= std_logic_vector(to_unsigned(2, 18));
+      WAIT FOR CLK_PERIOD;
+      
+      -- Third round (A + B + Previous Sum)
+      WAIT FOR CLK_PERIOD;
+      DATA_IN_A <= std_logic_vector(to_unsigned(3, 25));
+      DATA_IN_B <= std_logic_vector(to_unsigned(3, 18));
+      WAIT FOR CLK_PERIOD;
+      
+      -- Fourth round (A + B + Previous Sum)
+      WAIT FOR CLK_PERIOD;
+      DATA_IN_A <= std_logic_vector(to_unsigned(4, 25));
+      DATA_IN_B <= std_logic_vector(to_unsigned(4, 18));
+      WAIT FOR CLK_PERIOD;
+      
+      -- Finish simulation
+      WAIT FOR 50 ns;
+      WAIT;
+  END PROCESS;
 
 end Behavioral;
-
